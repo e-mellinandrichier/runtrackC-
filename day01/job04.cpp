b3f1949d8cf0c7ded0684b05547ab47cbd6d31eb
Emmanuelle Mellinand-Richier <mellinand.emmanuelle@gmail.com>
2025-05-12 15:54:13 +0200

    job04

---
+++ day01/job04.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+
+    int n1 = std::atoi(argv[1]);
+    int n2 = std::atoi(argv[2]);
+
+        std::cout << n1+n2;
+
+    return 0;
+}
